05e43714a95f68050ca28b3998182d3d94fb98c9
whl <wplovitz@gmail.com>
2022-12-07 16:43:24 -0800

    adding files fixing commit issues

---
+++ models/Idea.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const Inputs = require('./Input');
+
+const ideaSchema = new Schema(
+    {
+        ideaText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return `${date.toLocalDateString('en-us', { month: 'short'})} ${formatDay(date.getDate())}, ${date.getFullYear()} at ${date.roLocaleTimeString('en-us',)}`
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        Inputs: [Inputs],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    },
+);
+
+const formatDay = (day) => {
+    if (day > 3 && day < 21) return day+'th';
+    switch ( day % 10) {
+        case 1: return day+'st';
+        case 2: return day+"nd";
+        case 3: return day+"rd";
+        default: return day+"th";
+    };
+};
+
+ideaSchema.virtual('inputCount')
+    .get(function () {
+        return this.Inputs.length;
+    });
+
+const Idea = model('idea', ideaSchema);
+
+model.exports = Idea;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { default: mongoose } = require("mongoose");
+
+const { Schema, model, Types } = require(mongoose);
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /.+\@.+\..+/,
+        },
+        ideas: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Idea',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    },
+);
+
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+model.exports = User;
