c290f9e4ac5beb5791d1e68c8a191c7a4710d86c
whl <wplovitz@gmail.com>
2022-12-07 17:07:38 -0800

    adding input models

---
+++ models/Input.js
@@ -0,0 +1,44 @@
+const { Schema, types, Types } = require('mongoose');
+
+const inputSchema = new Schema(
+    {
+        inputId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        inputBody: {
+            type: String,
+            required: true,
+            max_length:280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return `${date.toLocalDateString('en-us', { month: 'short'})} ${formatDay(date.getDate())}, ${date.getFullYear()} at ${date.roLocaleTimeString('en-us',)}`
+            },
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+const formatDay = (day) => {
+    if (day > 3 && day < 21) return day+'th';
+    switch ( day % 10) {
+        case 1: return day+'st';
+        case 2: return day+"nd";
+        case 3: return day+"rd";
+        default: return day+"th";
+    };
+};
+
+module.exports =  inputSchema;
