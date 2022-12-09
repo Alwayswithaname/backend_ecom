da3230ebd4b4b4717e54bb5b14a6f7f3aa1f1cae
whl <wplovitz@gmail.com>
2022-12-08 16:46:50 -0800

    added creatUser and updateUser routs

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,5 +23,33 @@ module.exports = {
             res.json(err);
         };
     },
+
+    creatUser: async (req, res) => {
+        try {
+            const newUser = await User.creat(req.body);
+            res.json(newUser);
+        } catch (err) {
+            console.log('Error', err);
+            res.status(500).json(err);
+        }''
+    },
+    updateUser: async (req, res) => {
+        try {
+            const update = await User.findOneAndUpdate(
+                { _id: req.parms.userId },
+                { $set: req.body },
+                { runValidators: true, new: true},
+            );
+        if (!update) {
+            res.status(404).json({ message: 'User was not found' });
+        } else {
+            res.json(update);
+        };
+        } catch (err) {
+            console.log('Error', err);
+            res.status(500).json(err);
+        };
+    },
+
     
 }
